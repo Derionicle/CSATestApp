415c6f632ff830521f6c072f3bb82f7793b58d9b
Derionicle <nmerz@icloud.com>
2015-09-17 11:00:50 -0400

    personal sound file backup

--- app/src/main/java/com/example/nathan/csatestapp/MainActivity.java
+++ app/src/main/java/com/example/nathan/csatestapp/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.nathan.csatestapp;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        Button mButton1 = (Button)findViewById(R.id.button);
+        Button mButton2 = (Button) findViewById(R.id.button);
+        final MediaPlayer mpE = MediaPlayer.create(this, raw.scalee);
+        final MediaPlayer mpF = MediaPlayer.create(this, raw.scalef);
+        mButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("Main Activity", "Button 1 clicked");
+                mpE.start();
+            }
+        });
+        mButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("Main Activity", "Button 1 clicked");
+                mpF.start();
+            }
+        });
     }
 
 
